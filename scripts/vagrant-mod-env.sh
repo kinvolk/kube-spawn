636662de3d2e6573c75026069e213a98d64e688d
Dongsu Park <dongsu@kinvolk.io>
2017-07-10 13:44:36 +0200

    scripts: upgrade k8s to 1.7.0 for bugfixes
    We need to upgrade kubernetes from 1.6.6 to 1.7.0 to fix bugs such as
    kube-apiserver getting restarted continuously, as well as kube-proxy
    failing to write into /sys/module/nf_conntrack/parameters/hashsize.

--- scripts/vagrant-mod-env.sh
+++ scripts/vagrant-mod-env.sh
@@ -13,3 +13,7 @@ systemctl stop firewalld
 sudo groupadd docker && sudo gpasswd -a ${USER} docker && sudo systemctl restart docker && newgrp docker
 usermod -aG docker ${USER}
 sudo modprobe overlay
+
+NF_HASHSIZE=/sys/module/nf_conntrack/parameters/hashsize
+
+[ -f ${NF_HASHSIZE} ] && echo "131072" | sudo tee ${NF_HASHSIZE}
