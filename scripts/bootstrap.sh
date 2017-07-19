06075f76a9f2ff3c96292d8fc8e1e14a6b3f3eb7
Dongsu Park <dongsu@kinvolk.io>
2017-07-19 17:24:57 +0200

    scripts: mask systemd-networkd-wait-online.service
    Every time when docker.service runs, it waits for 120 seconds doing
    nothing, until systemd-networkd-wait-online.service times out.
    As a result, kubeadm is being blocked until docker server became
    available: a huge delay for every nspawn container, although network
    is already online and reachable.
    
    Let's just mask systemd-networkd-wait-online.service, to allow
    docker.service to run without being blocked.
    
    Fixes https://github.com/kinvolk/kube-spawn/issues/75

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,3 +7,6 @@ echo "root:k8s" | chpasswd
 systemctl enable docker.service
 systemctl enable kubelet.service
 systemctl enable sshd.service
+
+# necessary to prevent docker from being blocked.
+systemctl mask systemd-networkd-wait-online.service
