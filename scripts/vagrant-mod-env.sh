8a681a5321d942c56decfe0cb5cd9e018235438b
Dongsu Park <dongsu@kinvolk.io>
2017-07-19 16:05:08 +0200

    scripts: change USER handling and firewall policy
    Allow vagrant-mod-env to take $VUSER variable. as $USER becomes always
    "root" when running it with sudo command, we need to change the
    variable $USER to $VUSER, to be able to support different username
    for distros.
    
    Also change the default firewall policy for FORWARD chain
    especially for Debian systems.

--- scripts/vagrant-mod-env.sh
+++ scripts/vagrant-mod-env.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-set -euo pipefail
+# Run it with env variable $VUSER set to a customized user, other than the
+# default user "vagrant". For example on Ubuntu VM on Vagrant:
+#
+#  $ sudo VUSER=ubuntu ./vagrant-mod-env.sh
+
+set -eo pipefail
 
 if [ ${EUID} -ne 0 ]; then
 	echo "This script must be run as root"
 	exit 1
 fi
 
-USER=vagrant
-HOME=/home/${USER}
+if [ "${VUSER}" == "" ]; then
+	VUSER=vagrant
+fi
+
+set -u
+
+HOME=/home/${VUSER}
 
 echo 'Modifying environment'
 chmod +x ${HOME}/build.sh
 
 # setenforce always returns 1 when selinux is disabled.
 # we should ignore the error and continue.
-setenforce 0 || true
+/usr/sbin/setenforce 0 || true
+
+# Stop firewalld to allow traffic by default.
+# Note that especially on Debian systems, it's not sufficient to stop
+# firewalld, because the FORWARD chain's policy is still DROP.
 systemctl is-active firewalld 1>/dev/null && systemctl stop firewalld
-groupadd docker && gpasswd -a ${USER} docker && systemctl restart docker && newgrp docker
-usermod -aG docker ${USER}
+iptables -P FORWARD ACCEPT
+sysctl -w net.ipv4.ip_forward=1
+
+groupadd docker && gpasswd -a ${VUSER} docker && systemctl restart docker && newgrp docker
+usermod -aG docker ${VUSER}
 
 modprobe overlay
 modprobe nf_conntrack
